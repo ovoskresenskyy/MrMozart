d7cf8ee8f98c87a0fa64c78acb3a2b4529228224
ovoskresenskyi <7949894@ukr.net>
2023-02-27 11:40:35 +0200

    Init commit

---
+++ src/main/java/com/example/mzrt/MzrtApplication.java
@@ -0,0 +1,13 @@
+package com.example.mzrt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MzrtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MzrtApplication.class, args);
+    }
+
+}
