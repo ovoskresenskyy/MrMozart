44ff1f6c27096c0c1c9c895e3e9966d6a25dcad8
ovoskresenskyi <7949894@ukr.net>
2023-03-05 23:46:59 +0200

    Deleted all reminders of registration

--- src/main/java/com/example/mzrt/controller/AuthController.java
+++ src/main/java/com/example/mzrt/controller/AuthController.java
@@ -1,65 +1,18 @@
 package com.example.mzrt.controller;
 
-import com.example.mzrt.model.User;
-import com.example.mzrt.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.Optional;
 
 @Controller
 public class AuthController {
-
-    private final UserService userService;
-
-    @Autowired
-    public AuthController(UserService userService) {
-        this.userService = userService;
-    }
 
     @GetMapping("/login")
     public String login() {
         return "security/login";
     }
 
-    @GetMapping("/index")
+    @GetMapping
     public String index() {
-        return "index";
+        return "redirect:/list";
     }
-
-    @GetMapping("/register")
-    public String showRegistrationForm(Model model) {
-        //TODO: temporary unavailable to register
-//        model.addAttribute("user", User.builder()
-//                .build());
-//        return "security/register";
-        return "redirect:/login";
-    }
-
-    @PostMapping("/register/save")
-    public String registration(@ModelAttribute("user") User user,
-                               BindingResult result,
-                               Model model) {
-        //TODO: temporary unavailable to register
-//        Optional<User> existingUser = userService.findByEmail(user.getEmail());
-//        if (existingUser.isPresent()) {
-//            result.rejectValue("email",
-//                    "isExist",
-//                    "There is already an account registered with the same email");
-//        }
-//
-//        if (result.hasErrors()) {
-//            model.addAttribute("user", User.builder().build());
-//            return "redirect:/register?error";
-//        }
-//
-//        userService.saveUser(user, "ADMIN");
-        return "redirect:/login";
-    }
-
 }
