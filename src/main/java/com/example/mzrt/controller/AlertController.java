56086209689af4ccec853901aa4194b0b9836d2e
ovoskresenskyi <7949894@ukr.net>
2023-03-05 23:48:50 +0200

    Reworked alert controller

--- src/main/java/com/example/mzrt/controller/AlertController.java
+++ src/main/java/com/example/mzrt/controller/AlertController.java
@@ -2,60 +2,66 @@ package com.example.mzrt.controller;
 
 import com.example.mzrt.model.Alert;
 import com.example.mzrt.service.AlertService;
-import com.example.mzrt.service.OrderService;
+import com.example.mzrt.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping
+@RequestMapping("/alerts")
 public class AlertController {
 
-    private final OrderService orderService;
     private final AlertService alertService;
+    private final UserService userService;
 
     @Autowired
-    public AlertController(OrderService orderService, AlertService alertService) {
-        this.orderService = orderService;
+    public AlertController(AlertService alertService, UserService userService) {
         this.alertService = alertService;
+        this.userService = userService;
     }
 
-    @GetMapping("/orders")
-    public String getAllOrders(Model model) {
-        model.addAttribute("orders", orderService.findAll());
-        return "orders/list";
+    @GetMapping
+    public String redirectToUsers() {
+        return "redirect:/users";
     }
 
-    @GetMapping("/alerts/new")
-    public String alertNewForm(Model model) {
-        model.addAttribute("alert", Alert.builder().build());
-        return "alerts/new";
-    }
-
-    @GetMapping("/alerts")
-    public String getAllAlerts(Model model) {
-        model.addAttribute("alerts", alertService.findAll());
+    @GetMapping("/{userId}")
+    public String getAlerts(@PathVariable int userId,
+                            Model model) {
+        model.addAttribute("user", userService.findById(userId));
+        model.addAttribute("alerts", alertService.findByUserId(userId));
         return "alerts/list";
     }
 
-    @PostMapping("/alerts")
-    public String saveAlert(@ModelAttribute("alert") Alert alert) {
-        alertService.save(alert);
-        return "redirect:/alerts";
+    @GetMapping("/{userId}/new")
+    public String alertNewForm(@PathVariable int userId,
+            Model model) {
+        model.addAttribute("user", userService.findById(userId));
+        model.addAttribute("alert", Alert.builder()
+                .userId(userId)
+                .build());
+        return "alerts/new";
     }
 
-    @GetMapping("/alerts/{id}/updating")
-    public String alertUpdateForm(@PathVariable(value = "id") int id, Model model) {
+    @PostMapping
+    public String saveAlert(@ModelAttribute("alert") Alert alert) {
+        alertService.save(alert);
+        return "redirect:/alerts/" + alert.getUserId();
+    }
+
+    @GetMapping("/updating/{id}")
+    public String alertUpdateForm(@PathVariable int id, Model model) {
         Alert alert = alertService.findById(id);
+        model.addAttribute("user", userService.findById(alert.getUserId()));
         model.addAttribute("alert", alert);
         return "alerts/update";
     }
 
-    @DeleteMapping("/alerts/{id}")
-    public String deleteOwner(@PathVariable(value = "id") int id) {
+    @DeleteMapping("/{id}")
+    public String deleteOwner(@PathVariable int id) {
+        int ownerId = alertService.findById(id).getUserId();
         alertService.deleteById(id);
-        return "redirect:/alerts";
+        return "redirect:/alerts/" + ownerId;
     }
-
 }
