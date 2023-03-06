1b7ba2de37a070c7cdecd665c2521104ebbd89b2
ovoskresenskyi <7949894@ukr.net>
2023-03-06 15:29:07 +0200

    Disable security for parent pages

--- src/main/java/com/example/mzrt/config/SecurityConfiguration.java
+++ src/main/java/com/example/mzrt/config/SecurityConfiguration.java
@@ -33,8 +33,8 @@ public class SecurityConfiguration {
         http.csrf().disable()
                 .authorizeHttpRequests((authorize) ->
                         authorize
-                                .requestMatchers("/login", "/register/**", "/alert").permitAll()
-                                .requestMatchers("/orders", "/users", "alerts", "/").hasAnyAuthority("ADMIN", "USER")
+                                .requestMatchers("/login", "/alert", "/alert/**").permitAll()
+                                .requestMatchers("/orders/**", "/users/**", "/alerts/**", "/").hasAnyAuthority("ADMIN", "USER")
                                 .anyRequest().authenticated()
                                 .and()
                 ).formLogin(
