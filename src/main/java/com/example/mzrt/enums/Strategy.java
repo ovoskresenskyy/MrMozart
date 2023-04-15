9725270f322b992816c1e346b8c679204e5603b7
ovoskresenskyi <7949894@ukr.net>
2023-04-15 17:45:12 +0300

    Added enum for separating strategies

---
+++ src/main/java/com/example/mzrt/enums/Strategy.java
@@ -0,0 +1,12 @@
+package com.example.mzrt.enums;
+
+public enum Strategy {
+    MOZART("Mozart"),
+    BLACK_FLAG("Black Flag");
+
+    public final String name;
+
+    Strategy(String name) {
+        this.name = name;
+    }
+}
