88c93991dc27a317c71dae005be770845dadbee9
ovoskresenskyi <7949894@ukr.net>
2023-04-15 17:51:04 +0300

    Deal service.
    Added logic to get possibility find an opened deal.
    With processing alert, getting side and separating alert
    
    All found information saving into the deal

---
+++ src/main/java/com/example/mzrt/service/DealService.java
@@ -0,0 +1,73 @@
+package com.example.mzrt.service;
+
+import com.example.mzrt.model.Deal;
+import com.example.mzrt.repository.DealRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.stream.DoubleStream;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@Service
+public class DealService {
+
+    private final DealRepository dealRepository;
+
+    @Autowired
+    public DealService(DealRepository dealRepository) {
+        this.dealRepository = dealRepository;
+    }
+
+
+    public Deal findById(int id) {
+        return dealRepository.findById(id).orElseThrow(() -> new ResponseStatusException(NOT_FOUND));
+    }
+
+    public Deal getOpenedDealByTicker(int userId, String ticker, String side) {
+        Optional<Deal> existedDeal = dealRepository.getByUserIdAndTickerAndSideAndOpenTrue(userId, ticker, side);
+        return existedDeal.orElseGet(() -> dealRepository.save(Deal.builder()
+                .userId(userId)
+                .ticker(ticker)
+                .side(side)
+                .build()));
+    }
+
+    public void setPrice(Deal deal, int alertNumber, int price) {
+        switch (alertNumber) {
+            case 1 -> deal.setFirstPrice(price);
+            case 2 -> deal.setSecondPrice(price);
+            case 3 -> deal.setThirdPrice(price);
+            case 4 -> deal.setFourthPrice(price);
+            case 5 -> deal.setFifthPrice(price);
+        }
+        calculateAveragePrice(deal);
+    }
+
+    private void calculateAveragePrice(Deal deal){
+        OptionalDouble average = DoubleStream.of(deal.getFifthPrice(),
+                        deal.getSecondPrice(),
+                        deal.getThirdPrice(),
+                        deal.getFourthPrice(),
+                        deal.getFifthPrice())
+                .filter(price -> price != 0)
+                .average();
+
+        deal.setAveragePrice(average.isPresent() ? average.getAsDouble() : 0);
+        dealRepository.save(deal);
+    }
+
+    public boolean orderIsPresent(Deal deal, int alertNumber) {
+        return switch (alertNumber) {
+            case 1 -> deal.getFirstPrice() > 0;
+            case 2 -> deal.getSecondPrice() > 0;
+            case 3 -> deal.getThirdPrice() > 0;
+            case 4 -> deal.getFourthPrice() > 0;
+            case 5 -> deal.getFifthPrice() > 0;
+            default -> true;
+        };
+    }
+}
