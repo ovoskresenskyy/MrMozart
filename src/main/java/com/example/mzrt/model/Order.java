6ad4d67041de006fca68987b8533cbc349db0375
ovoskresenskyi <7949894@ukr.net>
2023-04-15 17:47:28 +0300

    Added deal id to connect with deal Added strategy to get possibility show all orders by the strategy Added price

--- src/main/java/com/example/mzrt/model/Order.java
+++ src/main/java/com/example/mzrt/model/Order.java
@@ -1,5 +1,6 @@
 package com.example.mzrt.model;
 
+import com.example.mzrt.enums.Strategy;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,13 +18,17 @@ public class Order {
 
     @Id
     private int id;
+    @Column("deal_id")
+    private int dealId;
     private String name;
     private String secret;
     private String side;
     private String symbol;
+    private String strategy;
     @Column("user_id")
     private int userId;
     private String timestamp;
     @Column("timestamp_sent")
     private String timestampSent;
+    private int price;
 }
