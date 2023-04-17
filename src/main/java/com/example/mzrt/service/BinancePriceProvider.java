427bbb3ae73eeacb468c2737215167bf11085918
ovoskresenskyi <7949894@ukr.net>
2023-04-17 15:06:31 +0300

    Price tracker set connection with binance and process returned data. Holding current price in field

---
+++ src/main/java/com/example/mzrt/service/BinancePriceProvider.java
@@ -0,0 +1,40 @@
+package com.example.mzrt.service;
+
+import com.binance.connector.futures.client.impl.UMWebsocketClientImpl;
+
+public class BinancePriceProvider {
+
+    private double price;
+    private int streamId;
+    private final UMWebsocketClientImpl client;
+
+    public BinancePriceProvider() {
+        this.client = new UMWebsocketClientImpl();
+    }
+
+    public void startTracking(String ticker) {
+        this.streamId = client.aggTradeStream(ticker, ((event) -> {
+            this.price = getPriceFromEvent(event);
+        }));
+    }
+
+    public void closeConnection() {
+        client.closeConnection(streamId);
+    }
+
+    private double getPriceFromEvent(String event) {
+        return Double.parseDouble(
+                event.substring(
+                                event.indexOf("p\":") + 4,
+                                event.indexOf("q\":") - 3)
+                        .replaceAll("\"", ""));
+    }
+
+    public double getPrice() {
+        double priceToReturn = 0;
+        while (priceToReturn == 0) { //TODO: need to rebuild this shit
+            priceToReturn = price;
+        }
+        return priceToReturn;
+    }
+}
