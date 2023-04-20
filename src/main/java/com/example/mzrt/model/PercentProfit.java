3c479d45ef651aef64d9d3a5b7cb8fe1ab799ed6
ovoskresenskyi <7949894@ukr.net>
2023-04-20 13:39:11 +0300

    New table. To collect percents of profit matched by strategy

---
+++ src/main/java/com/example/mzrt/model/PercentProfit.java
@@ -0,0 +1,24 @@
+package com.example.mzrt.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "percent_profit")
+public class PercentProfit {
+
+    @Id
+    private int id;
+    @Column("ticker_id")
+    private int tickerId;
+    @Column("strategy_id")
+    private int strategyId;
+}
