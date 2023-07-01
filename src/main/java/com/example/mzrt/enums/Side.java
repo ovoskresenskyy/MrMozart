128ed21f360f3a579346e58e0795c1d54a354310
ovoskresenskiy <7949894@ukr.net>
2023-07-02 00:29:43 +0200

    1. Added one more map into the enum of the Sides New deals will hold only name, so need to have possibility to get enum by the name.
    2. Added method to find the side in the map by the name
    
    3. Added comments
    
    4. Added new method to determine is the received side is short or long

--- src/main/java/com/example/mzrt/enums/Side.java
+++ src/main/java/com/example/mzrt/enums/Side.java
@@ -8,10 +8,12 @@ public enum Side {
     LONG("Long", "buy");
 
     private static final Map<String, Side> BY_ACTION = new HashMap<>();
+    private static final Map<String, Side> BY_NAME = new HashMap<>();
 
     static {
         for (Side e : values()) {
             BY_ACTION.put(e.action, e);
+            BY_NAME.put(e.name, e);
         }
     }
 
@@ -23,7 +25,37 @@ public enum Side {
         this.action = action;
     }
 
+    /**
+     * This method simply returns the value of enum by the received action (side)
+     *
+     * @param action - 'sell/buy'
+     * @return The enum if the matched side
+     */
     public static Side valueByAction(String action) {
         return BY_ACTION.get(action);
     }
+
+    /**
+     * This method simply returns the value of enum by the received name (side)
+     *
+     * @param name - 'Short/Long'
+     * @return The enum if the matched side
+     */
+    public static Side valueByName(String name) {
+        return BY_NAME.get(name);
+    }
+
+    /**
+     * This method is determines if the current side is a short pos ar a long.
+     *
+     * @param side - 'sell/buy' for the old deals, 'Short/Long' for the new one
+     * @return True if it's SHORT, false if it's LONG
+     */
+    public static boolean isShort(String side) {
+        Side sideByAction = valueByAction(side);
+        Side sideByName = valueByName(side);
+
+        return sideByAction == Side.SHORT || sideByName == Side.SHORT;
+    }
+
 }
