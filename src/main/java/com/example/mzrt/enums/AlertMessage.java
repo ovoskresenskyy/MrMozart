89757b4c0e6d5a49d72360be4f0109bd8b6e7997
ovoskresenskiy <7949894@ukr.net>
2023-07-02 23:29:58 +0200

    Added new enums to hold messages. Maybe will use it more later.

---
+++ src/main/java/com/example/mzrt/enums/AlertMessage.java
@@ -0,0 +1,45 @@
+package com.example.mzrt.enums;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum AlertMessage {
+
+    STOP_LINE_CHANGE("Stop Line Change");
+
+    private static final Map<String, AlertMessage> BY_NAME = new HashMap<>();
+
+    static {
+        for (AlertMessage e : values()) {
+            BY_NAME.put(e.name, e);
+        }
+    }
+
+    public final String name;
+
+    AlertMessage(String name) {
+        this.name = name;
+    }
+
+    /**
+     * This method simply returns the value of enum by the received name
+     *
+     * @param name - Name of the alert received in the message
+     * @return The enum if the matched side
+     */
+    private static AlertMessage valueByName(String name) {
+        return BY_NAME.get(name);
+    }
+
+
+    /**
+     * This method determines is the received message told to stop lossing the value
+     *
+     * @param message - Received message in the alert
+     * @return True if it's StopLoss alert, false if not
+     */
+    public static boolean isStopLoss(String message) {
+        return valueByName(message) == STOP_LINE_CHANGE;
+    }
+
+}
