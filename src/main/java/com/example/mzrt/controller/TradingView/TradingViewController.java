1e3c494c26358b94441e82bb956e6d3d99ff0dbd
ovoskresenskiy <7949894@ukr.net>
2023-07-02 23:05:34 +0200

    TradingViewController 1. Added comment for alert handle method 2. Move getting time into the strategy handling methods

--- src/main/java/com/example/mzrt/controller/TradingView/TradingViewController.java
+++ src/main/java/com/example/mzrt/controller/TradingView/TradingViewController.java
@@ -5,9 +5,6 @@ import com.example.mzrt.service.strategies.BlackFlagService;
 import com.example.mzrt.service.strategies.MozartService;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 @RestController
 @RequestMapping
 public class TradingViewController {
@@ -20,6 +17,27 @@ public class TradingViewController {
         this.mozartService = mozartService;
     }
 
+    /**
+     * This method receives the JSON message from the TradingView with the text message inside
+     * Handle it accordingly to the strategy.
+     * <p>
+     * Text can be:
+     * 1. Mozart
+     * - L1-L5 / S1-S5
+     * - LTP1-LTP5 / STP1-STP5
+     * - LSL, SSL
+     * - Stop trend
+     * 2. Black Flag
+     * - ~Price crossed above Fib1 level in long trend
+     * - Stop Line Change
+     *
+     * @param token    - Users 'id'
+     * @param strategy - Name of strategy
+     * @param contract - spot/futures
+     * @param ticker   - Pair of the coins like BTCUSDT
+     * @param message  - Additional text info
+     * @return - Created order if success or empty one if not
+     */
     @PostMapping(value = "/{token}/{strategy}/{contract}/{ticker}",
             consumes = {"text/plain", "application/*"},
             headers = "content-type=text/json",
@@ -29,13 +47,9 @@ public class TradingViewController {
                              @PathVariable(value = "contract") String contract,
                              @PathVariable(value = "ticker") String ticker,
                              @RequestBody String message) {
-
-        ticker = ticker.toUpperCase();
-        String alertTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
-
         return switch (strategy) {
-            case "bf" -> blackFlagService.handleAlert(token, message, ticker, alertTime);
-            case "moz" -> mozartService.handleAlert(token, message, ticker, alertTime);
+            case "bf" -> blackFlagService.handleAlert(token, message, ticker.toUpperCase());
+            case "moz" -> mozartService.handleAlert(token, message, ticker.toUpperCase());
             default -> Order.builder().build();
         };
     }
