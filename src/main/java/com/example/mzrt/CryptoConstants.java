6b120132b9900b0e61f43d4936c4f19cc0ed53e6
ovoskresenskiy <7949894@ukr.net>
2023-07-04 00:28:46 +0200

    Added one more constant

--- src/main/java/com/example/mzrt/CryptoConstants.java
+++ src/main/java/com/example/mzrt/CryptoConstants.java
@@ -2,7 +2,12 @@ package com.example.mzrt;
 
 public class CryptoConstants {
 
+    /**
+     * ID of once deal for that strategy
+     */
+    public static int MOZART_DEAL_ID = 2;
+
     public static int MOZART_STRATEGY_ID = 1;
-    public static int BF_STRATEGY_ID = 1;
+    public static int BF_STRATEGY_ID = 2;
 
 }
