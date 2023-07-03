895690201b30c029444fb3f44e3fe9b82e9bcc6e
ovoskresenskiy <7949894@ukr.net>
2023-07-03 23:47:45 +0200

    Added new field into the deal model - last_change_time This field will hold the time of the last changing of the deal

--- src/main/resources/db/migration/V1_33__Add_Deal_Timestamp.sql
+++ src/main/resources/db/migration/V1_33__Add_Deal_Timestamp.sql
@@ -1,6 +1,6 @@
 ALTER TABLE deal
-    ADD IF NOT EXISTS last_change_time timestamp;
+    ADD IF NOT EXISTS last_change_time text;
 
 UPDATE deal
-SET last_change_time = now()
+SET last_change_time = '01-01-2023 23:59:59'
 WHERE deal.last_change_time IS NULL;
