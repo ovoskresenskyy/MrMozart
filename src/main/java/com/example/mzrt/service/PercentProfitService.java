658b59f015fca4c4d715a20f02ca5af52677a4aa
ovoskresenskiy <7949894@ukr.net>
2023-07-11 16:59:44 +0200

    Added comments for the getPercent methods

--- src/main/java/com/example/mzrt/service/PercentProfitService.java
+++ src/main/java/com/example/mzrt/service/PercentProfitService.java
@@ -17,6 +17,17 @@ public class PercentProfitService {
         this.tickerService = tickerService;
     }
 
+    /**
+     * This method is responsible for getting the instance of PercentProfit
+     * according to the received parameters,
+     * or creating the new one if not found
+     * <p>
+     * It's held separately for each strategy and each ticker
+     *
+     * @param strategyId - The ID of the strategy
+     * @param tickerId   - The ID of the ticker
+     * @return - Instance of the PercentProfit
+     */
     public PercentProfit getPercentProfit(int strategyId, int tickerId) {
         return percentProfitsRepository.findByStrategyIdAndTickerId(strategyId, tickerId)
                 .orElse(PercentProfit.builder()
@@ -25,7 +36,14 @@ public class PercentProfitService {
                         .build());
     }
 
-    public double getPercent(Deal deal){
+    /**
+     * This method is responsible for getting the value of exist PercentProfit instance
+     * be the received deal. The deal hold the information about the strategy and the ticker.
+     *
+     * @param deal - The deal as a holder of the strategy and ticker of the percent profit.
+     * @return The percent profit of the received ticker for the given strategy
+     */
+    public double getPercent(Deal deal) {
         int strategyId = deal.getStrategyId();
         int tickerId = tickerService.findByNameAndUserId(deal.getTicker(), deal.getUserId()).getId();
 
